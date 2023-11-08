4569592f378bc678f711ee85ebc7ace9e025731e
Aminah <aminahpsaleem@gmail.com>
2023-11-08 09:37:19 +0000

    sets column length to -0

--- main.py
+++ main.py
@@ -31,7 +31,11 @@ def get_slot_machine_spin(rows, cols, symbols):
             
             columns.append(column)
        
-       return columns      
+    return columns  
+   
+def print_slot_machine(columns):
+       for row in range(len(columns[0])):
+           
 
 
 def deposit():

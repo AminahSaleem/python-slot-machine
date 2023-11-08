3c8c8752911d9fedf88b31797025b8986315e5e5
Aminah <aminahpsaleem@gmail.com>
2023-11-08 10:01:33 +0000

    creates a loop so the game can keep playing

--- main.py
+++ main.py
@@ -129,6 +129,16 @@ def main():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
     print(f"You won on lines:", *winning_lines)
+    return winning_lines - total_bet
+
+def main():
+    balance= deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        spin = input("Press enter to spin (q to quit).")
+        if spin == "q":
+            break
+        balance += spin()
    
 main() 
     
